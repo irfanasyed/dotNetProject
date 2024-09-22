719cb0b959e7e298f2028be3bdd5675f3652396a
irfanasyed <132118395+irfanasyed@users.noreply.github.com>
2024-09-22 23:07:20 +0530

    Add files via upload

---
+++ JaggedArray.cs
@@ -0,0 +1,43 @@
+﻿int[][] weekSales = new int[4][];
+Console.WriteLine(" the sales of every week");
+weekSales[0] =new int[4] {450,500,1200,300};
+weekSales[1] = new int[5] {560,4500,750,340,890};
+weekSales[2] = new int[7] {300,150,250,400,670,500,1000};
+weekSales[3] = new int[6] {680,900,470,300,800,150};
+for (int i = 0; i < weekSales.Length; i++)
+{
+    Console.Write("WeekSales[{0}]\t",i+1+" ");
+    
+    for (int j = 0; j < weekSales[i].Length; j++)
+    {
+        
+        
+        Console.Write(weekSales[i][j]+" ");
+        
+    }
+    Console.WriteLine();
+    
+}
+Console.WriteLine("the total of each week");
+int sum = 0;
+int totalSum = 0;
+int k = 0;
+for ( k= 0; k < weekSales.Length; k++)
+{
+    for (int j = 0; j < weekSales[k].Length; j++)
+    {
+        sum +=weekSales[k][j];
+
+    }
+   
+    Console.WriteLine("weekSales[{0}]: {1}", k + 1, sum);
+    totalSum = totalSum + sum;
+
+
+    sum = 0;
+
+}
+Console.WriteLine("The total sales"+totalSum);
+
+
+
---
+++ MultiDimensionalArrayForStudentMark.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Enter number of students:");
+int noOfStudent = Convert.ToInt32(Console.ReadLine());
+string[,] student = new string[noOfStudent, 2];
+int[,] mark = new int[noOfStudent, 6];
+string[,] result = new string[noOfStudent, 1];
+for (int i = 0; i < noOfStudent; i++)
+{
+    Console.WriteLine("Enter the details of students:");
+    Console.WriteLine("Enter Student name: ");
+    student[i, 0] = Console.ReadLine();
+    Console.WriteLine("Enter the student ID:");
+    student[i, 1] = Console.ReadLine();
+    Console.WriteLine("Enter english mark:");
+    mark[i, 0] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter tamil mark:");
+    mark[i, 1] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter maths mark:");
+    mark[i, 2] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter science mark:");
+    mark[i, 3] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter social mark:");
+    mark[i, 4] = Convert.ToInt32(Console.ReadLine());
+    mark[i, 5] = mark[i, 0] + mark[i, 1] + mark[i, 2] + mark[i, 3] + mark[i, 4];
+    result[i, 0] = mark[i, 5] > 250 ? "Pass" : "Fail";
+}
+
+for (int i = 0; i < noOfStudent; i++)
+{
+    Console.WriteLine("Details of students: {0}", i + 1);
+    Console.WriteLine("");
+    Console.WriteLine($"Student name: {student[i, 0]}");
+    Console.WriteLine($"Student ID: {student[i, 1]}");
+    Console.WriteLine($"English mark: {mark[i, 0]}");
+    Console.WriteLine($"Tamil mark: {mark[i, 1]}");
+    Console.WriteLine($"Maths mark: {mark[i, 2]}");
+    Console.WriteLine($"Science mark: {mark[i, 3]}");
+    Console.WriteLine($"Social mark: {mark[i, 4]}");
+    Console.WriteLine($"Total : {mark[i, 5]}");
+    Console.WriteLine($"Result: {result[i, 0]}");
+
+
+}
